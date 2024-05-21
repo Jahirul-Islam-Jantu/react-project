0c9dbe130c888ebe5fea6012064f8eea32ee6013
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-05-21 17:13:23 +0600

    hook managment with button

--- my-app/src/reactHook/UseRef.jsx
+++ my-app/src/reactHook/UseRef.jsx
@@ -2,10 +2,14 @@ import { useRef } from "react";
 
 const UseRef = () => {
   let myHeadline = useRef();
-  myHeadline.current.innerHtml = "Hello React Hook!";
+
+  const change = () => {
+    myHeadline.current.innerHTML = "Hello React Hook!";
+  };
   return (
     <div>
       <h1 ref={myHeadline}>My React hook Headline</h1>
+      <button onClick={change}>Click here</button>
     </div>
   );
 };
