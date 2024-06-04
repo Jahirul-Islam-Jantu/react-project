d3ace19ed4493005c2618b45a9b085264716a713
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-06-04 23:51:15 +0600

    todo app

--- my-app/src/reactHook/TodoApp.jsx
+++ my-app/src/reactHook/TodoApp.jsx
@@ -7,10 +7,14 @@ const TodoApp = () => {
     list.push(item);
     setList([...list]);
   };
+
+  const removItem = (index) => {
+    list.splice(index, 1);
+    setList([...list]);
+  };
+
   return (
     <div>
-      <h1 className="text-center">{list.length}</h1>
-
       <div className="input-group mb-3 row g-3 text-center">
         <span className="input-group-text col-auto rounded">
           {" "}
@@ -25,6 +29,31 @@ const TodoApp = () => {
           Add Item
         </button>
       </div>
+
+      <table>
+        <tbody>
+          {list.length !== 0 ? (
+            list.map((element, index) => {
+              return (
+                <tr>
+                  <td>{element}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        removItem(index);
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr></tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
