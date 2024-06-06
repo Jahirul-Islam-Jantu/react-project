ad28d386d3f97d1957a5f34b40133ab1fb03a3d2
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-06-06 09:47:26 +0600

    todo app completed

--- my-app/src/reactHook/NewToDo.jsx
+++ my-app/src/reactHook/NewToDo.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
+
 const NewToDo = () => {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const addItem = () => {
+    list.push(item);
+    setList([...list]);
+  };
+  const removeItem = (index) => {
+    list.splice(index, 1);
+    setList([...list]);
+  };
+
   return (
     <div>
-      <input type="text" placeholder="Item" /> <br />
-      <button>ADD Item</button>
+      <input
+        type="text"
+        placeholder="Item"
+        className="p-2 mb-2"
+        onChange={(e) => setItem(e.target.value)}
+      />{" "}
+      <br />
+      <button onClick={addItem}>ADD Item</button>
+      <table>
+        <tbody>
+          {list.length !== 0 ? (
+            list.map((element, index) => {
+              return (
+                <tr>
+                  <td>{element}</td>
+                  <td>
+                    <button onClick={() => removeItem(index)}>Remove</button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr></tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
